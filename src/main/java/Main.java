5dba40855e2f91a5e296c790bd7e200a6ec766fb
Laneige90 <kjw1243@naver.com>
2023-08-17 12:39:42 +0900

    JAVA

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        int number = 1;
-        System.out.println(number);
+        // write your code here
+
+        short s = 1;
+        System.out.println(s);
+
+        int a = 3;
+        System.out.println(a);
+
+        long b = 4;
+        System.out.println(b);
+
+        float f = 5.5F;
+        System.out.println(f);
+
+        double d = 5.5;
+        System.out.println(d);
+
+        System.out.println(Short.MAX_VALUE);
+
+        System.out.println((Short.MIN_VALUE));
     }
 }
