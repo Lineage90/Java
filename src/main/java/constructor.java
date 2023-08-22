0326b431018c56a9988b1af468c687510db62e71
Laneige90 <kjw1243@naver.com>
2023-08-22 17:32:25 +0900

    JAVA

---
+++ src/main/java/constructor.java
@@ -0,0 +1,15 @@
+class Phone {
+   String model;
+   String color;
+   int price;
+
+    public Phone(String model, String color, int price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+}
+
+public class constructor {
+
+}
